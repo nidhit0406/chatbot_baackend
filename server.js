9777139bb52e829cb1e2a03f5b2acc33679f6c32
nidhi <nidhit0406@gmail.com>
2025-07-29 10:16:26 +0530

    minor change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: ['*'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  // credentials: true
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
